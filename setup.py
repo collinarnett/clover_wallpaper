47f3ff26f910159da2a6374b5fd451be423ed12d
Collin Arnett <collin@arnett.it>
2020-04-14 16:40:57 +0000

    v0.1

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='cloverwallpaper',
+    packages=['cloverwallpaper'],
+    version='0.1',
+    description='A tool to download wallpapers from 4chan.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Collin Arnett',
+    author_email='collin@arnett.it',
+    url='https://github.com/collinarnett/clover_wallpaper',
+    keywords=['4chan', 'wallpaper', 'downloader', 'image', 'scraper'],
+    install_requires=[
+        'requests',
+        'tqdm',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Operating System :: OS Independent',
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.6'
+)
